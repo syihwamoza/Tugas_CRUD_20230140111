bd9932f899e7ee2e512b50eed581a98f5c439a16
syihwamoza <mozakastella26@gmail.com>
2026-03-10 17:24:41 +0700

    last commit

---
+++ src/main/java/com/moza/tugas3/Tugas3Application.java
@@ -0,0 +1,13 @@
+package com.moza.tugas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/moza/tugas3/Tugas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moza.tugas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
